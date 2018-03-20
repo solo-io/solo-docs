7bf79748b1d9112bf4dfbe87f812025280de7302
Ashish Shrestha <axhixh@gmail.com>
2018-03-20 12:22:12 -0400

    tag builder image with version

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -t soloio/gloo-docs:latest .
+export TAG=`git log -n 1 --pretty=format:%h`
+docker build -t soloio/gloo-docs:latest -t soloio/gloo-docs:$TAG .
 docker push soloio/gloo-docs:latest
+docker push soloio/gloo-docs:$TAG
